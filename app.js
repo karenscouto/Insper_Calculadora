046d54cc8507f9ee9498990bde28969d4e28cd99
Karen Couto <karenscouto@gmail.com>
2022-06-03 15:02:24 -0300

    up

--- app.js
+++ app.js
@@ -6,20 +6,17 @@ let salarios = [
   {
     "de": 0,
     "ate": 1212,
-    "percentual": "55%", 
-    "mensagem": "Cerca de 55% dos jogadores profissionais ganham 1 salário mínimo. Pode ser um bom negócio calçar as chuteiras!"
+    "mensagem": "Cerca de 55% dos jogadores profissionais ganham 1 salário mínimo. Calçar as chuteiras pode ser um bom negócio!"
   },
   {
     "de": 1212,
     "ate": 5000,
-    "percentual": "33%",
     "mensagem": "Somente 33% dos jogadores ganham entre R$ 1.212,00 a R$ 5.000,00. Pode ser escolha difícil..."
   },
   {
     "de": 5000,
     "ate": Infinity,
-    "percentual": "12%",
-    "mensagem": "Tem que ter muita bola no pé para entrar no grupo seleto dos 12% dos jogadores mais bem pagos"
+    "mensagem": "Tem que ter muita bola no pé para entrar no grupo seleto dos 12% dos jogadores mais bem pagos."
   },
   
 ]
